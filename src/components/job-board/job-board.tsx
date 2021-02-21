167141eca4323e5fdebcdf5c8675747e339a944b
Kacper Krzywiec <krzywiec@gmail.com>
2021-02-21 08:35:10 +0100

    Filter composition

--- src/components/job-board/job-board.tsx
+++ src/components/job-board/job-board.tsx
@@ -1,10 +1,11 @@
 import { FunctionComponent, useState, useEffect } from "react";
+import { compose, identity } from "ramda";
 import { JobOfferShort } from "components/job-offer-short";
 import { FavoriteOffersFilter } from "components/favorite-offers-filter";
 import { LocationFilter } from "components/location-filter";
 import useDebounce from "hooks/use-debounce";
 import { JobOffer, Company } from "common/interfaces";
-import { Input } from "../input";
+import { Input } from "components/input";
 import { jobOffersData, companiesData } from "common/data";
 import { localStorageFacade } from "common/helpers";
 import styles from "./styles.module.scss";
@@ -12,31 +13,49 @@ import styles from "./styles.module.scss";
 const findCompanyByID = (companyId: string, companiesData: Company[]) =>
   companiesData.find((company) => company.key === companyId);
 
-const filterBySearchTerm = (
-  jobOffers: JobOffer[],
-  companiesData: Company[],
-  term: string
-) => {
-  return jobOffers.filter(
-    (jobOffer) =>
-      (findCompanyByID(jobOffer.companyId, companiesData)?.companyName ?? "")
-        .toLowerCase()
-        .includes(term.toLowerCase()) ||
-      jobOffer.jobTitle.toLowerCase().includes(term.toLowerCase())
-  );
+interface Filter {
+  jobOffers: JobOffer[];
+  companiesData?: Company[];
+  term?: string;
+  favoriteOffers?: string[];
+  location?: string;
+}
+
+const filterBySearchTerm = (filterData: Filter): Filter => {
+  const { jobOffers, companiesData = [], term = "" } = filterData;
+  return {
+    ...filterData,
+    jobOffers: jobOffers.filter(
+      (jobOffer) =>
+        (findCompanyByID(jobOffer.companyId, companiesData)?.companyName ?? "")
+          .toLowerCase()
+          .includes(term.toLowerCase()) ||
+        jobOffer.jobTitle.toLowerCase().includes(term.toLowerCase())
+    ),
+  };
 };
 
 const locations = (jobOffers: JobOffer[]) =>
   Array.from(new Set(jobOffers.map((jobOffer) => jobOffer.location)));
 
-const filterByFavorite = (jobOffers: JobOffer[], favoriteOffers: string[]) => {
-  return jobOffers.filter((jobOffer) => favoriteOffers.includes(jobOffer.key));
+const filterByFavorite = (filterData: Filter): Filter => {
+  const { jobOffers, favoriteOffers = [] } = filterData;
+  return {
+    ...filterData,
+    jobOffers: jobOffers.filter((jobOffer) =>
+      favoriteOffers.includes(jobOffer.key)
+    ),
+  };
 };
 
-const filterByLocation = (jobOffers: JobOffer[], location: string) => {
-  return jobOffers.filter(
-    ({ location: offersLocation }) => offersLocation === location
-  );
+const filterByLocation = (filterData: Filter): Filter => {
+  const { jobOffers, location } = filterData;
+  return {
+    ...filterData,
+    jobOffers: jobOffers.filter(
+      ({ location: offerLocation }) => offerLocation === location
+    ),
+  };
 };
 
 export const JobBoard: FunctionComponent = () => {
@@ -46,30 +65,42 @@ export const JobBoard: FunctionComponent = () => {
     localStorageFacade.getFavoriteJobOffers()
   );
   const [location, setLocation] = useState<string>("");
-  const [filterFavoriteOn, setFilterFavoriteOn] = useState(false);
-  const debouncedSearchTerm = useDebounce(searchTerm, 500);
+  const [filterFavoriteOn, setFilterFavoriteOn] = useState<boolean>(false);
+  const debouncedSearchTerm = useDebounce<string>(searchTerm, 500);
+  const filterBySearchTermFunction =
+    debouncedSearchTerm !== "" ? filterBySearchTerm : identity;
+  const filterByFavoriteFunction = filterFavoriteOn
+    ? filterByFavorite
+    : identity;
+  const FilterByLocationFunction =
+    location !== "" ? filterByLocation : identity;
   useEffect(() => {
     setJobOffers(
-      filterBySearchTerm(jobOffersData, companiesData, debouncedSearchTerm)
+      compose(
+        filterBySearchTermFunction,
+        filterByFavoriteFunction,
+        FilterByLocationFunction
+      )({
+        jobOffers: jobOffersData,
+        companiesData,
+        term: debouncedSearchTerm,
+        favoriteOffers,
+        location,
+      }).jobOffers
     );
-  }, [debouncedSearchTerm]);
-  useEffect(() => {
-    if (filterFavoriteOn) {
-      setJobOffers(filterByFavorite(jobOffersData, favoriteOffers));
-    } else {
-      setJobOffers(jobOffersData);
-    }
-  }, [filterFavoriteOn, favoriteOffers]);
+  }, [
+    filterBySearchTermFunction,
+    filterByFavoriteFunction,
+    FilterByLocationFunction,
+    debouncedSearchTerm,
+    filterFavoriteOn,
+    favoriteOffers,
+    location,
+  ]);
   useEffect(() => {
     localStorageFacade.updateFavoriteJobOffers(favoriteOffers);
   }, [favoriteOffers]);
-  useEffect(() => {
-    if (location) {
-      setJobOffers(filterByLocation(jobOffersData, location));
-    } else {
-      setJobOffers(jobOffersData);
-    }
-  }, [location]);
+
   const onChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) =>
     setSearchTerm(event.target.value);
   return (
